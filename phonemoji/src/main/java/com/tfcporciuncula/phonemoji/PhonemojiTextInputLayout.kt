db5c54eff73421e961c6b51bcb0160f50809e670
Ketan <ketan@sensara.tv>
2020-09-13 00:17:28 +0530

    Revert "textinputlayout padding changes"
    This reverts commit a77f779d06e8ecdf6169d920623ca4f6de06e133.

--- phonemoji/src/main/java/com/tfcporciuncula/phonemoji/PhonemojiTextInputLayout.kt
+++ phonemoji/src/main/java/com/tfcporciuncula/phonemoji/PhonemojiTextInputLayout.kt
@@ -1,12 +1,9 @@
 package com.tfcporciuncula.phonemoji
 
 import android.content.Context
-import android.graphics.Rect
 import android.util.AttributeSet
 import com.google.android.material.textfield.TextInputLayout
 import com.tfcporciuncula.phonemoji.internal.TextDrawable
-import java.lang.reflect.InvocationTargetException
-import java.lang.reflect.Method
 
 /**
  * Works with a [PhonemojiTextInputEditText] child and renders an emoji flag as a start icon based on its input.
@@ -53,74 +50,5 @@ class PhonemojiTextInputLayout : TextInputLayout {
     val phonemojiEditText = editText as? PhonemojiTextInputEditText
     checkNotNull(phonemojiEditText) { "PhonemojiTextInputLayout requires a PhonemojiTextInputEditText child" }
     PhonemojiHelper.watchPhoneNumber(phonemojiEditText) { startIconDrawable = TextDrawable(it, flagSize) }
-  }
-
-  private var bounds: Rect? = null
-  private var recalculateMethod: Method? = null
-  private var collapsingTextHelper: Any? = null
-
-  /**
-   * Companion Object
-   */
-  companion object {
-    const val COLLAPSING_HELPER = "collapsingTextHelper"
-    const val COLLAPSED_BOUNDS = "collapsedBounds"
-    const val RECALCULATE = "recalculate"
-  }
-
-  init {
-    init2()
-  }
-
-  private fun init2() {
-    try {
-      //Search internal and private class over object called as variable
-      val cthField = TextInputLayout::class.java.getDeclaredField(COLLAPSING_HELPER)
-      cthField.isAccessible = true
-      collapsingTextHelper = cthField.get(this)
-
-      //Search in private class the other component to create a view
-      val boundsField = collapsingTextHelper?.javaClass?.getDeclaredField(COLLAPSED_BOUNDS)
-      boundsField?.isAccessible = true
-      bounds = boundsField?.get(collapsingTextHelper) as Rect
-      recalculateMethod = collapsingTextHelper?.javaClass?.getDeclaredMethod(RECALCULATE)
-
-    } catch (e: NoSuchFieldException) {
-      collapsingTextHelper = null
-      bounds = null
-      recalculateMethod = null
-      e.printStackTrace()
-    } catch (e: IllegalAccessException) {
-      collapsingTextHelper = null
-      bounds = null
-      recalculateMethod = null
-      e.printStackTrace()
-    } catch (e: NoSuchMethodException) {
-      collapsingTextHelper = null
-      bounds = null
-      recalculateMethod = null
-      e.printStackTrace()
-    }
-  }
-
-  override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-    super.onLayout(changed, left, top, right, bottom)
-    adjustBounds()
-  }
-
-  private fun adjustBounds() {
-    if (collapsingTextHelper == null)
-      return
-    try {
-      bounds?.left = editText?.left!! + editText?.paddingLeft!!
-      recalculateMethod?.invoke(collapsingTextHelper)
-    } catch (e: InvocationTargetException) {
-      e.printStackTrace()
-    } catch (e: IllegalAccessException) {
-      e.printStackTrace()
-    } catch (e: IllegalArgumentException) {
-      e.printStackTrace()
-    }
-
   }
 }
