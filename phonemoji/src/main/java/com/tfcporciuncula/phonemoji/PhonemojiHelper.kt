cbb370a6ba3ed231e4c42436a2c6d608f3056047
Fred Porciúncula <fred.porciuncula@nufin.de>
2020-11-02 09:56:26 +0100

    Bump dependencies

--- phonemoji/src/main/java/com/tfcporciuncula/phonemoji/PhonemojiHelper.kt
+++ phonemoji/src/main/java/com/tfcporciuncula/phonemoji/PhonemojiHelper.kt
@@ -23,29 +23,31 @@ object PhonemojiHelper {
     var currentCountryCode = editText.initialCountryCode
     onCountryChanged(regionCodeToEmoji(phoneNumberUtil.getRegionCodeForCountryCode(currentCountryCode)))
 
-    editText.addTextChangedListener(object : TextWatcher {
-      override fun afterTextChanged(s: Editable) {
-        if (s.contains(" ")) { // First space is added when country code is identified.
-          // If we can't get the country code between '+' (thus index 1) and the first space, we give up.
-          val countryCode = runCatching { s.substring(1, s.indexOf(" ")).toInt() }.getOrNull() ?: return
+    editText.addTextChangedListener(
+      object : TextWatcher {
+        override fun afterTextChanged(s: Editable) {
+          if (s.contains(" ")) { // First space is added when country code is identified.
+            // If we can't get the country code between '+' (thus index 1) and the first space, we give up.
+            val countryCode = runCatching { s.substring(1, s.indexOf(" ")).toInt() }.getOrNull() ?: return
 
-          // If it's the same as the current one, no need to bother.
-          if (currentCountryCode == countryCode) return
+            // If it's the same as the current one, no need to bother.
+            if (currentCountryCode == countryCode) return
 
-          // Otherwise, we get the region code for the country code, so we can translate it to a flag emoji.
-          val regionCode = phoneNumberUtil.getRegionCodeForCountryCode(countryCode)
+            // Otherwise, we get the region code for the country code, so we can translate it to a flag emoji.
+            val regionCode = phoneNumberUtil.getRegionCodeForCountryCode(countryCode)
 
-          // If we can't get a valid region code, we give up.
-          if (regionCode == UNKNOWN_REGION) return
+            // If we can't get a valid region code, we give up.
+            if (regionCode == UNKNOWN_REGION) return
 
-          currentCountryCode = countryCode
-          onCountryChanged(regionCodeToEmoji(regionCode))
+            currentCountryCode = countryCode
+            onCountryChanged(regionCodeToEmoji(regionCode))
+          }
         }
-      }
 
-      override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) = Unit
-      override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) = Unit
-    })
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) = Unit
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) = Unit
+      }
+    )
   }
 
   // https://stackoverflow.com/a/35849652/2352699
